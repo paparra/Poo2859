908a624ed32a675e8b563b72fb8c8b4171f879de
kechafla <78629506+kechafla@users.noreply.github.com>
2021-02-18 22:58:54 -0500

    Add files via upload

---
+++ Poo2859-67d514eb66e833265773a0a67be39f062284f42c/HolaMundo.java
@@ -0,0 +1,13 @@
+public class HolaMundo{
+public static void main(String[] args){
+String valor="Hola mundo";
+System.out.println(valor);
+System.out.println(valor);
+System.out.println(valor.length());
+if(valor.length()%2==0){
+            System.out.println("El numero es par");
+}else{
+            System.out.println("El numero es inpar");
+        }
+}
+}
